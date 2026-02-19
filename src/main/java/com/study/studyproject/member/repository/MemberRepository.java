3ca410106a8ad5855ca1dfacb4417076dd131c02
jacomyou0121 <jacomyou1026@naver.com>
2026-02-19 18:04:19 +0900

    [refactor] MemberRepository 수정

--- src/main/java/com/study/studyproject/member/repository/MemberRepository.java
+++ src/main/java/com/study/studyproject/member/repository/MemberRepository.java
@@ -4,6 +4,8 @@ package com.study.studyproject.member.repository;
 import com.study.studyproject.member.domain.Member;
 import com.study.studyproject.member.domain.SocialType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,7 +14,13 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
 
     Optional<Member> findByEmail(String email);
-    Optional<Member> findByNickname(String email);
+    Optional<Member> findByNickname(String nickname);
+
+    @Query("select m.email from Member m where m.nickname = :nickname")
+    Optional<String> findEmailByNickname(@Param("nickname")  String nickname);
+    @Query("select m.email from Member m where m.id = :id")
+    Optional<String> findEmailById(@Param("id") Long id);
+
 
     boolean existsByNickname(String nickname);
 
